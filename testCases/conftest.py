bb62a9fc428e8f6560044e2599b408e4d10c7fbc
Eddie4k-code <98666468+Eddie4k-code@users.noreply.github.com>
2023-01-19 17:31:29 -0500

    Removed unused function.

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -54,37 +54,6 @@ def pytest_configure(config):
     config._metadata["Tester"] = "Eddie"
 
 
-"""
-@pytest.hookimpl(hookwrapper=True)
-def pytest_runtest_makereport(item, setup):
-    driver = setup()
-    pytest_html = item.config.pluginmanager.getplugin("html")
-    outcome = yield
-    report = outcome.get_result()
-    extra = getattr(report, "extra", [])
-    if report.when == "call":
-        # always add url to report
-        extra.append(pytest_html.extras.url("http://www.example.com/"))
-        xfail = hasattr(report, "wasxfail")
-        if (report.skipped and xfail) or (report.failed and not xfail):
-
-
-            # only add additional html on failure
-            report_directory = os.path.dirname(item.config.option.htmlpath)
-            file_name = report.nodeid.replace("::", "_") + ".png"
-            destinationFile = os.path.join(report_directory, file_name)
-            self.driver.save_screenshot(destinationFile)
-
-            if file_name:
-                html = '<div><img src="%s" alt="screenshot" style="width:300px;height=200px" onclick="window.open(this.src)" align="right" />' % file_name
-
-
-
-
-
-            extra.append(pytest_html.extras.html(html))
-        report.extra = extra
-"""
 
 def pytest_html_report_title(report):
     report.title = "Automation"
